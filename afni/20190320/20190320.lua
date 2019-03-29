5f9f8aea0d2e1e80a2e1068c7537298b86b6943d
Gabriel A. Devenyi <gdevenyi@gmail.com>
2019-03-28 21:29:09 -0400

    Fix bin path for afni

--- afni/20190320/20190320.lua
+++ afni/20190320/20190320.lua
@@ -3,4 +3,4 @@ conflict(myModuleName())
 local quarantinepath    = os.getenv("QUARANTINE_PATH")
 local basepath = pathJoin(quarantinepath,myModuleName(),myModuleVersion(),"install")
 
-prepend_path( "PATH",           pathJoin(basepath,"linux_ubuntu_16_64/bin"))
+prepend_path( "PATH",           pathJoin(basepath,"linux_ubuntu_16_64"))

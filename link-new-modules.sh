e4fe1ebdf687abc28884b25c171a9b5744d40bea
Gabriel A. Devenyi <gdevenyi@gmail.com>
2019-04-01 22:02:57 -0400

    Update link new modules to create modules for new programs too

--- link-new-modules.sh
+++ link-new-modules.sh
@@ -2,6 +2,10 @@
 
 shopt -s nullglob
 
+for dir in $(find . -maxdepth 1 -mindepth 1 -type d | grep -v -E '(resources|modules|.git)'); do
+	mkdir -p modules/${dir}
+done
+
 for dir in modules/*; do
 	for file in $(basename ${dir})/*/*lua; do
 		(cd ${dir} && cp -s ../../${file} . )

2f0870150ee007401c878962fa274ba791ac7610
Gabriel A. Devenyi <gdevenyi@gmail.com>
2019-04-12 23:42:20 -0400

    Fix antspath

--- ANTs/20190318/20190318.lua
+++ ANTs/20190318/20190318.lua
@@ -4,4 +4,4 @@ local quarantinepath    = os.getenv("QUARANTINE_PATH")
 local basepath = pathJoin(quarantinepath,myModuleName(),myModuleVersion(),"install")
 
 prepend_path( "PATH",           pathJoin(basepath,"bin"))
-setenv( "ANTSPATH", pathJoin(basepath,"bin/") )
+setenv( "ANTSPATH", pathJoin(basepath,"bin") )

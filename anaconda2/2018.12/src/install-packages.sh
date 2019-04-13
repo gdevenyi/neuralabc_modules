80aa2153986d8e7f75624a1eea26d2d2193750c3
Gabriel A. Devenyi <gdevenyi@gmail.com>
2019-04-12 23:42:37 -0400

    Update package list for anaconda

--- anaconda2/2018.12/src/install-packages.sh
+++ anaconda2/2018.12/src/install-packages.sh
@@ -1,3 +1,7 @@
-for package in nipype nibabel simpleitk dipy jupyterlab nilearn bokeh plotly keras tensorflow theano statsmodels seaborn numba pytorch; do
+for package in nipype nibabel simpleitk dipy jupyterlab nilearn bokeh \
+		plotly keras tensorflow theano statsmodels seaborn numba \
+		pytorch jcc tqdm multiprocess pathos vtk seaborn; do
 	conda install --no-update-deps ${package}
 done
+
+pip install qbatch

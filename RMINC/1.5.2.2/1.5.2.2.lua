960b21bf542ab72ca235d9cf715c438628dc354f
Gabriel A. Devenyi <gdevenyi@gmail.com>
2019-06-12 16:18:14 -0400

    Update dependencies on RMINC module

--- RMINC/1.5.2.2/1.5.2.2.lua
+++ RMINC/1.5.2.2/1.5.2.2.lua
@@ -1,6 +1,7 @@
 conflict(myModuleName())
 
 prereq("R")
+prereq("minc-toolkit-v2/1.9.17")
 
 local quarantinepath    = os.getenv("QUARANTINE_PATH")
 local basepath = pathJoin(quarantinepath,myModuleName(),myModuleVersion(),"install")

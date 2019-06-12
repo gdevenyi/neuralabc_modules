a1f4b44aa4a745a62e6bbbb1870158b1f7d43542
Gabriel A. Devenyi <gdevenyi@gmail.com>
2019-06-12 16:18:46 -0400

    Add mango

---
+++ mango/4.1/4.1.lua
@@ -0,0 +1,8 @@
+conflict(myModuleName())
+
+local quarantinepath    = os.getenv("QUARANTINE_PATH")
+local basepath = pathJoin(quarantinepath,myModuleName(),myModuleVersion(),"install")
+
+prepend_path( "PATH",           basepath)
+prepend_path( "PATH",           pathJoin(basepath,"lib"))
+prepend_path( "PATH",           pathJoin(basepath,"jre7"))

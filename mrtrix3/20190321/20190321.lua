72710c1898d667ad809990e7ecccbd2048c4e517
Gabriel A. Devenyi <gdevenyi@gmail.com>
2019-09-06 16:01:08 -0400

    included modified mrtrix3 lua file

--- mrtrix3/20190321/20190321.lua
+++ mrtrix3/20190321/20190321.lua
@@ -4,3 +4,4 @@ local quarantinepath    = os.getenv("QUARANTINE_PATH")
 local basepath = pathJoin(quarantinepath,myModuleName(),myModuleVersion(),"install")
 
 prepend_path( "PATH",           pathJoin(basepath,"bin"))
+prepend_path( "PATH",           pathJoin(quarantinepath,"mrtrix3_resources"))

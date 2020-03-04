2a9a12988b667b991a45e40aa55b9a657b4fe2f6
Gabriel A. Devenyi <gdevenyi@gmail.com>
2020-03-04 12:30:57 -0500

    updated istall packages

--- anaconda2/2018.12/src/install-packages.sh
+++ anaconda2/2018.12/src/install-packages.sh
@@ -1,6 +1,9 @@
 for package in mkl nipype nibabel simpleitk dipy jupyterlab nilearn bokeh \
 		plotly keras tensorflow theano statsmodels seaborn numba \
-		pytorch jcc tqdm multiprocess pathos vtk seaborn pytables zarr; do
+		pytorch jcc tqdm multiprocess pathos vtk seaborn pytables zarr \
+		pyqt scikit-learn pillow pytest-cov pytest-mock pytest-timeout flake8 \
+		pyface traitsui pyvista mayavi PySurfer neo pytest-faulthandler \
+		pytest-sugar pydocstyle codespell python-picard; do
 	conda install --no-update-deps ${package} --yes
 done
 

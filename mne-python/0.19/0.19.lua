8f1fc7604693f5f735d4da5ca4c26148571abad8
Gabriel A. Devenyi <gdevenyi@gmail.com>
2020-03-04 13:19:14 -0500

    added cupy to anaconda

--- mne-python/0.19/0.19.lua
+++
@@ -1,7 +0,0 @@
-prereq("anaconda3")
-conflict(myModuleName())
-
-local quarantinepath    = os.getenv("QUARANTINE_PATH")
-local basepath = pathJoin(quarantinepath,myModuleName(),myModuleVersion(),"install")
-
-prepend_path( "PYTHONPATH",           pathJoin(basepath,"lib/python3.7/site-packages"))

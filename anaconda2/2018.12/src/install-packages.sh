b11ad2afceaa9c05cbb055ec44dc0ff390721fba
Gabriel A. Devenyi <gdevenyi@gmail.com>
2020-06-18 17:57:44 -0400

    added datalad

--- anaconda2/2018.12/src/install-packages.sh
+++ anaconda2/2018.12/src/install-packages.sh
@@ -3,7 +3,8 @@ for package in mkl nipype nibabel simpleitk dipy jupyterlab nilearn bokeh \
 		pytorch jcc tqdm multiprocess pathos vtk seaborn pytables zarr \
 		pyqt scikit-learn pillow pytest-cov pytest-mock pytest-timeout flake8 \
 		pyface traitsui pyvista mayavi PySurfer neo pytest-faulthandler \
-		pytest-sugar pydocstyle codespell python-picard cupy r-base r-essentials; do
+		pytest-sugar pydocstyle codespell python-picard cupy r-base r-essentials \
+		datalad; do
 	conda install --no-update-deps ${package} --yes
 done
 
